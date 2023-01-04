74c2d17d2554fe6cbb389f6b960ae4294566aa28
Kwaku28 <rekenburg777@gmail.com>
2023-01-04 14:22:02 +0000

    Menu responsiveness for easier navigation

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ vectors.addEventListener('click', () => {
   navLink.classList.toggle('active');
 });
 
-document.querySelectorAll('ul.nav-links > li').forEach((n) => n.
-  addEventListener('click', () => {
+document.querySelectorAll('ul.nav-links > li').forEach((n) => n
+  .addEventListener('click', () => {
     vectors.classList.remove('active');
     navLink.classList.remove('active');
   }));
