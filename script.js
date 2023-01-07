915be7219b5cf8b6d0babbd108b69bba9201f964
Kwaku28 <113942699+Kwaku28@users.noreply.github.com>
2023-01-07 23:52:20 +0000

    Add code to display the popup window

--- script.js
+++ script.js
@@ -16,27 +16,6 @@ const openModelButtons = document.querySelectorAll('[data-model-target]');
 const closeModelButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
-openModelButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const model = document.querySelector(button.dataset.modelTarget);
-    openModel(model);
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const models = document.querySelectorAll('.model.active')
-  models.forEach(model => {
-    closeModel(model);
-  })
-})
-
-closeModelButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const model = button.closest('.model');
-    closeModel(model);
-  })
-})
-
 function openModel(model) {
   if (model == null) return;
   model.classList.add('active');
@@ -48,3 +27,24 @@ function closeModel(model) {
   model.classList.remove('active');
   overlay.classList.remove('active');
 }
+
+openModelButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const model = document.querySelector(button.dataset.modelTarget);
+    openModel(model);
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const models = document.querySelectorAll('.model.active')
+  models.forEach(model => {
+    closeModel(model);
+  });
+});
+
+closeModelButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const model = button.closest('.model');
+    closeModel(model);
+  });
+});
