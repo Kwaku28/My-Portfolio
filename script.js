7900821a41df9145d5b380346db3aadff8451cc4
Kwaku28 <rekenburg777@gmail.com>
2023-01-07 13:53:08 +0000

    update

--- script.js
+++ script.js
@@ -48,3 +48,41 @@ function closeModel(model) {
   model.classList.remove('active');
   overlay.classList.remove('active');
 }
+
+const firstName = document.getElementById('fname');
+const lastName = document.getElementById('lname');
+const emailEl = document.getElementById('email');
+const textArea = document.getElementById('type');
+const form = document.getElementById('form');
+
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+  let messages = [];
+
+  const isRequired = value => value === '' ? false : true;
+
+  function checkEmail() {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+      showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+      showError(emailEl, 'Email is not valid.');
+    } else {
+      showSuccess(emailEl);
+      valid = true;
+    }
+    return valid;
+  }
+
+  const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+    return re.test(email);
+  };
+
+  if (messages,length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ');
+  }
+})
